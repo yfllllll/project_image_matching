a21a4453096e4c0ee42847062e8a4b8fcc5a90df
yfllllll <yanfeiliu@whu.edu.cn>
2025-09-16 18:57:05 +0800

    fix

--- test.py
+++ test.py
@@ -17,7 +17,6 @@ from poslocation.Coarse_img import SmartImage
 from matching.viz import *
 from matching import get_matcher
 import time
-import logging  
 from enum import Enum  
 from contextlib import contextmanager  
 import asyncio  
@@ -154,9 +153,9 @@ rotation_angle_step = 360 / rotation_steps
 class LocationParam(BaseModel):
     """单个定位参数模型"""
     ref_image: str
-    homography: List[List[float]]
-    transform: List[float]  # [a, b, c, d, e, f]
-    crs_src: str
+    homography: List[List[float]] | None
+    transform: List[float] | None # [a, b, c, d, e, f] 
+    crs_src: str|None
     info_for_project_point_to_orthoimage: Optional[Dict] = None
 
 class GeoLocationRequest(BaseModel):
@@ -295,8 +294,20 @@ def compute_homography_and_warp_dynamic(src_image, H_final):
     warped_src = cv2.warpPerspective(src_image, H_translated, (new_width, new_height))
     return warped_src, x_min, y_min, H_translated
 
+
+
+# def get_crs_transform_from_tif(tif_path):   
+#     with rasterio.open(tif_path) as ref:
+#         base_transform = ref.transform
+#         crs = ref.crs
+#         xres = base_transform.a
+#         yres = base_transform.e
+#         transform = rasterio.Affine(
+#             xres, 0, base_transform.c + x_offset * xres,
+#             0, yres, base_transform.f + y_offset * yres
+#         )
 def save_as_geotiff(image_array, save_path, ref_tif_path=None, x_offset=0, y_offset=0):
-    height, width, channels = image_array.shape
+
 
     if ref_tif_path:
         with rasterio.open(ref_tif_path) as ref:
@@ -311,14 +322,16 @@ def save_as_geotiff(image_array, save_path, ref_tif_path=None, x_offset=0, y_off
     else:
         crs = rasterio.crs.CRS.from_epsg(4326)
         transform = from_origin(0, 0, 1, 1)
-    with rasterio.open(
-        save_path, 'w', driver='GTiff',
-        height=height, width=width,
-        count=channels, dtype=image_array.dtype,
-        crs=crs, transform=transform
-    ) as dst:
-        for i in range(channels):
-            dst.write(image_array[:, :, i], i + 1)
+    if image_array is not None:
+        height, width, channels = image_array.shape
+        with rasterio.open(
+            save_path, 'w', driver='GTiff',
+            height=height, width=width,
+            count=channels, dtype=image_array.dtype,
+            crs=crs, transform=transform
+        ) as dst:
+            for i in range(channels):
+                dst.write(image_array[:, :, i], i + 1)
     return transform, crs
 
 
@@ -426,7 +439,7 @@ async def geolocate_endpoint(
                             result["transform"].a, result["transform"].b,   
                             result["transform"].c, result["transform"].d,  
                             result["transform"].e, result["transform"].f  
-                        ],  
+                        ] if result["transform"] is not None else None,
                         crs_src=result["crs_src"],  
                         info_for_project_point_to_orthoimage=convert_numpy_to_list(  
                             result.get("info_for_project_point_to_orthoimage")  
@@ -582,11 +595,15 @@ def locate(drone_image_path, remote_tile_dir, top_k=1):
             src_image = camera_img.get_coarse_tif() if camera_img else cv2.imread(src_path)
             ref_image = cv2.imread(ref_path)
             H = image_matching(src_image, ref_image, isCameraImg=isCameraImg,resize=resize)
-            if H is None:
-                continue
-            warped_src, x_min, y_min, H = compute_homography_and_warp_dynamic(src_image, H)
-            output_tif_path = os.path.join('tmpdir', f"warped_{os.path.basename(src_path)}.tif")
-            transform, crs = save_as_geotiff(warped_src, output_tif_path, ref_path, x_offset=x_min, y_offset=y_min)
+            if H is not None:
+                warped_src, x_min, y_min, H = compute_homography_and_warp_dynamic(src_image, H)
+                output_tif_path = os.path.join('tmpdir', f"warped_{os.path.basename(src_path)}.tif")
+                transform, crs = save_as_geotiff(warped_src, output_tif_path, ref_path, x_offset=x_min, y_offset=y_min)
+            else:
+                transform = None
+                H = None
+                output_tif_path = None
+                crs = None
             results.append({
                 "src_image": src_path,
                 "ref_image": ref_path,
@@ -638,24 +655,19 @@ def project_point_to_ortho(eo, R, focal_length, pixel_size, boundary, gsd, groun
     x_ortho = (X_ground - min_x) / gsd
     y_ortho = (max_y - Y_ground) / gsd  
     
-    return x_ortho[0], y_ortho[0]
+    return x_ortho[0], y_ortho[0], X_ground, Y_ground
 
 def pixel_to_geo(pixel_xy, entry):
     """坐标转换实现"""
     geo_coords = []
     projected_points = []
     
-    H = np.array(entry.homography)
+    if entry.homography is None:
+        H = None
+    else:
+        H = np.array(entry.homography)
     transform_vals = entry.transform  # [a, b, c, d, e, f]
-    if H is None or transform_vals is None:
-        geo_coords.append(None)
-        projected_points.append(None)
-        return geo_coords, projected_points
-
-    transform = rasterio.Affine(*transform_vals)
-
-    # Step 1: 单应性变换后像素坐标
-    if entry.info_for_project_point_to_orthoimage is not None: 
+    if entry.info_for_project_point_to_orthoimage is not None: # 无人机影像的元数据都在且可以根据元数据进行粗定位
         info = entry.info_for_project_point_to_orthoimage
         R = np.array(info['R']).reshape(3, 3)
         focal_length = info['focal_length']
@@ -666,13 +678,33 @@ def pixel_to_geo(pixel_xy, entry):
         image_shape = info['image_shape']
         u, v = pixel_xy
         eo = info['eo'][:3]
-        new_x, new_y = project_point_to_ortho(
+        new_x, new_y, X_ground, Y_ground = project_point_to_ortho(
             eo, R, focal_length, pixel_size,
             boundary, gsd, ground_height, image_shape, u, v
         )
         pixel_xy = [new_x, new_y]
-        
-    
+        if H is None:
+            map_x, map_y = X_ground, Y_ground
+            projected_points.append((map_x, map_y))
+            # Step 3: 地图坐标系 → 经纬度坐标
+            with rasterio.open(entry.ref_image) as dataset:
+                crs_src = dataset.crs  # 当前影像 CRS
+            crs_dst = "EPSG:4326"     # 目标坐标系 WGS84
+            transformer = Transformer.from_crs(crs_src, crs_dst, always_xy=True)
+            lon, lat = transformer.transform(map_x, map_y)
+
+            geo_coords.append((lon, lat))    
+            return geo_coords, projected_points
+
+
+    if transform_vals is None: # 无人机的元数据不存在，且没有正确匹配
+        geo_coords.append(None)
+        projected_points.append(None)
+        return geo_coords, projected_points
+
+    transform = rasterio.Affine(*transform_vals)
+
+    # Step 1: 单应性变换后像素坐标
     pt = np.array([pixel_xy[0], pixel_xy[1], 1.0]).reshape(3, 1)
     pt_transformed = H @ pt
     pt_transformed /= pt_transformed[2]  # 归一化
